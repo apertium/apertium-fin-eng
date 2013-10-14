ca89fc09d287d9499b9421036932f32e1eea260f
Tommi Pirinen <flammie@iki.fi>
2013-10-14 13:41:40 +0000

    Props and advs too

--- dev/debug2dixesandlexces.bash
+++ dev/debug2dixesandlexces.bash
@@ -8,7 +8,11 @@ if test ! -r $1 ; then
     echo Cannot test -r $1
     exit 1
 fi
+apertium -d . fin-eng-debug < $1 | egrep -o '\*[^ .:,]*' | sort | uniq | sed -e 's/^.*$/\0%<XXX%>:\0 # ;/' >> apertium-fin-eng.fin.lexc
 apertium -d . fin-eng-debug < $1 | egrep -o '@[^ .:,]*'  | sed -e 's/><.*/>/' -e 's/<\([^>]*\)>/<s n="\1"\/>/g' -e 's/@/    <e><p><l>/' -e 's:$:</l><r>XXX<s n="Y"/></r></p></e>:' | sort | uniq >> apertium-fin-eng.fin-eng.dix
 
 apertium -d . fin-eng-debug < $1 | egrep -o '#[^ .:,]*' | fgrep '<n>' | sed -e 's/#//' -e 's/<.*//' | sort | uniq | sed -e 's/^.*$/\0%<n%>:\0 N ;/' >> apertium-fin-eng.eng.lexc
+apertium -d . fin-eng-debug < $1 | egrep -o '#[^ .:,]*' | fgrep '<np>' | sed -e 's/#//' -e 's/<.*//' | sort | uniq | sed -e 's/^.*$/\0%<np%>:\0 PROPN ;/' >> apertium-fin-eng.eng.lexc
 apertium -d . fin-eng-debug < $1 | egrep -o '#[^ .:,]*' | fgrep '<vblex>' | sed -e 's/#//' -e 's/<.*//' | sort | uniq | sed -e 's/^.*$/\0%<vblex%>:\0 V ;/' >> apertium-fin-eng.eng.lexc
+apertium -d . fin-eng-debug < $1 | egrep -o '#[^ .:,]*' | egrep '<(pr|post)>' | sed -e 's/#//' -e 's/<.*//' | sort | uniq | sed -e 's/^.*$/\0%<pr%>:\0 # ;/' >> apertium-fin-eng.eng.lexc
+
