63e0a475fd17e6fe61bbd72824301892b98d188d
Tommi Pirinen <flammie@iki.fi>
2014-01-16 23:34:12 +0000

    few more

--- dev/debug2dixesandlexces.bash
+++ dev/debug2dixesandlexces.bash
@@ -8,7 +8,10 @@ if test ! -r $1 ; then
     echo Cannot test -r $1
     exit 1
 fi
-apertium -d . fin-eng-debug < $1 | egrep -o '\*[^ .:,]*' | sort | uniq | sed -e 's/^.*$/\0%<XXX%>:\0 # ;/' >> apertium-fin-eng.fin.lexc
+
+# fin.lexc is already at languages/apertium-fin
+#apertium -d . fin-eng-debug < $1 | egrep -o '\*[^ .:,]*' | sort | uniq | sed -e 's/^.*$/\0%<XXX%>:\0 # ;/' >> apertium-fin-eng.fin.lexc
+
 apertium -d . fin-eng-debug < $1 | egrep -o '@[^ .:,]*'  | sed -e 's/><.*/>/' -e 's/<\([^>]*\)>/<s n="\1"\/>/g' -e 's/@/    <e><p><l>/' -e 's:$:</l><r>XXX<s n="Y"/></r></p></e>:' | sort | uniq >> apertium-fin-eng.fin-eng.dix
 
 apertium -d . fin-eng-debug < $1 | egrep -o '#[^ .:,]*' | fgrep '<n>' | sed -e 's/#//' -e 's/<.*//' | sort | uniq | sed -e 's/^.*$/\0%<n%>:\0 N ;/' >> apertium-fin-eng.eng.lexc
