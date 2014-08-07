aa13214e2ad8c3c5bbc0efc573209b6ee7013de4
Francis M. Tyers <ftyers@prompsit.com>
2014-08-07 17:48:33 +0000

    update script

--- dev/diffs-to-eng-ref.sh
+++ dev/diffs-to-eng-ref.sh
@@ -14,8 +14,8 @@ echo Original results are in analdiffs
 diffstat analdiffs
 
 echo -e 'Multichar_Symbols\n\n%<n%>\n\n\nLEXICON Root\n\nNUMS ; \n' > /tmp/tagdict.lexc
-cat $1/*.handtagged* | sed  's/\//\t/1'  | sed 's/\^//g' | sed 's/\$//g' |\
-    sed 's/</%</g' | sed 's/>/%>/g' | awk '{print $2":"$1}' |\
+cat $1/*.handtagged* | grep -v -e 'WRONG' -e 'ERROR' -e '##' | sed  's/\//\t/1'  | sed 's/\^//g' | sed 's/\$//g' |\
+    sed 's/ /% /g' | sed 's/</%</g' | sed 's/>/%>/g' | awk -F'\t' '{print $2":"$1}' |\
     sed 's/$/ # ;/g' | sort -u | grep -v '\*' | grep -v '"' |\
     grep -P '[a-zA-Z].*:[a-zA-Z].*' >> /tmp/tagdict.lexc
 echo -e 'LEXICON NUMS\n\n<[%0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9]+>  DIGITLEX ;\n\nLEXICON DIGITLEX\n\n%<num%>%<sg%>: # ; ' >> /tmp/tagdict.lexc 
