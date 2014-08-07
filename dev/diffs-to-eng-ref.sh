fa59f2ce055402e7d3f162cba1ea0cebd152097c
Tommi Pirinen <flammie@iki.fi>
2014-08-07 17:15:52 +0000

    spectie's scripts

--- dev/diffs-to-eng-ref.sh
+++ dev/diffs-to-eng-ref.sh
@@ -12,3 +12,23 @@ for f in  $1/*.raw.txt ; do
 done
 echo Original results are in analdiffs
 diffstat analdiffs
+
+echo -e 'Multichar_Symbols\n\n%<n%>\n\n\nLEXICON Root\n' > /tmp/tagdict.lexc
+cat $1/*.handtagged* | sed  's/\//\t/1'  | sed 's/\^//g' | sed 's/\$//g' |\
+    sed 's/</%</g' | sed 's/>/%>/g' | awk '{print $2":"$1}' |\
+    sed 's/$/ # ;/g' | sort -u | grep -v '\*' | grep -v '"' |\
+    grep -P '[a-zA-Z].*:[a-zA-Z].*' >> /tmp/tagdict.lexc
+
+hfst-lexc -v -d /tmp/tagdict.lexc |\
+    hfst-invert |\
+    hfst-fst2fst -f olw -o /tmp/tagdict.hfst
+cat $1/*.handtagged* | cut -f2 -d'^' | cut -f1 -d'/' |\
+    sort -f | uniq -c | sort -gr  | apertium-destxt |\
+    hfst-proc -w /tmp/tagdict.hfst  | apertium-retxt  > /tmp/list.handtagged
+cat $1/*.handtagged* | cut -f2 -d'^' | cut -f1 -d'/' |\
+                   sort -f | uniq -c | sort -gr  | apertium-destxt |\
+                   lt-proc -w eng-fin.automorf.bin |\
+                   apertium-retxt  > /tmp/list.fineng
+diff -Naur /tmp/list.handtagged /tmp/list.fineng > listdiffs
+echo some better diffs in listdiffs
+diffstat listdiffs
