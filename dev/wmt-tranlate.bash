233f8f036ea1385d00c34054cf8e7b45f262c3ea
Flammie Pirinen <flammie@iki.fi>
2019-04-11 01:34:12 +0200

    remove stray analysis tags

--- dev/wmt-tranlate.bash
+++ dev/wmt-tranlate.bash
@@ -4,4 +4,5 @@ if test $# != 2 ; then
     exit 1
 fi
 sed -e 's/$/§/g' < $2 | apertium -d . $1 | tr -s '#@*§' '    ' |\
-    sed -e 's:/[^,.:? -]*::'
+    sed -e 's:/[^,.:? -]*::' |\
+    sed -e 's/<[^>]*>//g'
